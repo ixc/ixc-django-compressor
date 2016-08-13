3e1f356a90a7b965147ff6f731f744b32bf4d344
Tai Lee <real.human@mrmachine.net>
2016-08-13 12:20:53 +1000

    Add metadata for PyPI.

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
+from __future__ import print_function
+
 import setuptools
+import sys
+
+# Convert README.md to reStructuredText.
+if {'bdist_wheel', 'sdist'}.intersection(sys.argv):
+    try:
+        import pypandoc
+    except ImportError:
+        print('WARNING: You should install `pypandoc` to convert `README.md` '
+              'to reStructuredText to use as long description.',
+              file=sys.stderr)
+    else:
+        print('Converting `README.md` to reStructuredText to use as long '
+              'description.')
+        long_description = pypandoc.convert('README.md', 'rst')
 
 setuptools.setup(
     name='ixc-django-compressor',
     use_scm_version={'version_scheme': 'post-release'},
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/ixc-django-compressor',
+    description='Improvements to django-compressor.',
+    long_description=locals().get('long_description', ''),
+    license='MIT',
+    packages=setuptools.find_packages(),
     py_modules=['ixc_compressor'],
+    include_package_data=True,
     setup_requires=['setuptools_scm'],
 )
